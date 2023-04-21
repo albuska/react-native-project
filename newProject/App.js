0a59c3832edb22a2eddfb5dcbe89f12a3d82c63e
Albina Anistratenko <albuska2013@gmail.com>
2023-04-21 23:35:15 +0300

    Update App.js

--- newProject/App.js
+++ newProject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!! What is your name?</Text>
+      <Text>Hello world!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
