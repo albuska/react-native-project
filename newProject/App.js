861eaf13c7eaf10981a57a21ce0617d366506fb3
Albina Anistratenko <albuska2013@gmail.com>
2023-04-22 00:27:30 +0300

    Update App.js

--- newProject/App.js
+++ newProject/App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const[login, setLogin] = useState('');
+
+const handleLoginChange = (login) => {
+  setLogin(login); 
+}
   return (
     <View style={styles.container}>
-      <Text>Hello world!!!</Text>
+      <Text style={styles.title}>Реєстрація</Text>
+      <View>
+        <TextInput placeholder='Введіть свій логін' value={login} onChange={handleLoginChange} />
+         <TextInput placeholder='Введіть адресу електронної пошти' value={login} onChange={handleLoginChange}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +27,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    // fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: 30, 
+    lineHeight: 35, 
+    textAlign: 'center', 
+    letterSpacing: 0.01,  
+    color: '#212121',
+  }
 });
+
+
