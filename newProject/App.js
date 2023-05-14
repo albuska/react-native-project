4c190cac011af3df7ae8b256c2b5d9730f29811e
Albina Anistratenko <albuska2013@gmail.com>
2023-05-14 23:20:32 +0300

    start 2 homework

--- newProject/App.js
+++ newProject/App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , TextInput} from 'react-native';
-import React, {useState} from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
-  const[login, setLogin] = useState('');
+  const [login, setLogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const handleLoginChange = (login) => {
-  setLogin(login); 
-}
+  const handleLoginChange = (login) => {
+    setLogin(login);
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Реєстрація</Text>
       <View>
-        <TextInput placeholder='Введіть свій логін' value={login} onChange={handleLoginChange} />
-         <TextInput placeholder='Введіть адресу електронної пошти' value={login} onChange={handleLoginChange}/>
+        <TextInput
+          placeholder="Логін"
+          value={login}
+          onChange={handleLoginChange}
+        />
+        <TextInput
+          placeholder="Адреса електронної пошти"
+          value={email}
+          onChange={handleLoginChange}
+        />
+        <TextInput
+          placeholder="Пароль"
+          value={password}
+          onChange={handleLoginChange}
+        />
+        <Text>Показати</Text>
       </View>
       <StatusBar style="auto" />
+      <Button title="Зареєструватися" color="#FF7F50" />
     </View>
   );
 }
@@ -23,20 +40,18 @@ const handleLoginChange = (login) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     // fontFamily: 'Roboto',
-    fontStyle: 'normal',
+    fontStyle: "normal",
     fontWeight: 500,
-    fontSize: 30, 
-    lineHeight: 35, 
-    textAlign: 'center', 
-    letterSpacing: 0.01,  
-    color: '#212121',
-  }
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
 });
-
-
